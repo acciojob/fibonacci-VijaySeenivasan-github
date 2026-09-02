d370e169b52eef071394b4a4a226ae85521cda48
VijaySeenivasan-github <146552024+VijaySeenivasan-github@users.noreply.github.com>
2026-09-02 16:17:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
 // your code here
+	 if (num === 1) {
+        return 0;
+    }
+
+    let first = 0;
+    let second = 1;
+	let result = 0;
+
+	for (let index = 2; index < num; index++) {
+		result = first+ second;
+		first = second;
+		second = result;
+	}
+
+    return result
 }
 
 module.exports = fibonacci;
